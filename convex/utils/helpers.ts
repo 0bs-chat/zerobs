2a9c9601ecb4609dd7895e10a9c2778eff3ecd11
BarrelOfLube <mantrakp@gmail.com>
2025-07-12 01:19:34 -0700

    refactor: update chat action to use a simplified argument structure and remove user ID dependency; enhance helper functions for authentication and URL retrieval

--- convex/utils/helpers.ts
+++ convex/utils/helpers.ts
@@ -1,44 +1,9 @@
-import type { Doc } from "../_generated/dataModel";
+import type { Doc, Id } from "../_generated/dataModel";
 import {
   type QueryCtx,
   type MutationCtx,
   type ActionCtx,
-  mutation,
-  query,
-  action,
 } from "../_generated/server.js";
-
-export const withUser = <Ctx extends QueryCtx | MutationCtx | ActionCtx, Args extends [any] | [], Output>(
-  func: (ctx: Ctx & { userId: string }, ...args: Args) => Promise<Output>
-): ((ctx: Ctx, ...args: Args) => Promise<Output>) => {
-  return async (ctx: Ctx, ...args: Args) => {
-    const identity = await ctx.auth.getUserIdentity();
-    if (!identity) {
-      throw new Error(
-        'Unauthenticated call to function requiring authentication'
-      );
-    }
-    return func({ ...ctx, userId: identity.subject }, ...args);
-  };
-};
-
-export const actionWithUserId = <Args extends [any] | [], Output>(
-  func: (ctx: ActionCtx & { userId: string }, ...args: Args) => Promise<Output>
-) => {
-  return action(withUser(func));
-};
-
-export const queryWithUserId = <Args extends [any] | [], Output>(
-  func: (ctx: QueryCtx & { userId: string }, ...args: Args) => Promise<Output>
-) => {
-  return query(withUser(func));
-};
-
-export const mutationWithUserId = <Args extends [any] | [], Output>(
-  func: (ctx: MutationCtx & { userId: string }, ...args: Args) => Promise<Output>
-) => {
-  return mutation(withUser(func));
-};
 
 export async function getUrl(
   ctx: ActionCtx | MutationCtx,
@@ -50,3 +15,11 @@ export async function getUrl(
     return key;
   }
 }
+
+export const requireAuth = async (ctx: ActionCtx | MutationCtx | QueryCtx) => {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) {
+    throw new Error('Unauthenticated call to function requiring authentication');
+  }
+  return { userId: identity.subject };
+};
