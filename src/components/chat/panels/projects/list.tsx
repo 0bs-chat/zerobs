799098f2d8f1163d8afa147abfc437a9c0625e5b
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-07-15 22:32:51 +0530

    fix: project bug fix

--- src/components/chat/panels/projects/list.tsx
+++ src/components/chat/panels/projects/list.tsx
@@ -62,6 +62,8 @@ export const ProjectsList = () => {
                   setSelectedProjectId(
                     selectedProjectId === project._id ? null : project._id
                   );
+                } else {
+                  setSelectedProjectId(project._id);
                 }
               }}
             >
